7398c0ca28ad4dda43e47c565938df1239e6bc2d
CopperAnt750 <opmailt@gmail.com>
2021-06-17 18:07:30 +0300

    adding list of musicm, setList + sout

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,9 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
     private Music music;
     private String name;
     private int volume;
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
 
     public String getName() {
         return name;
@@ -30,7 +38,11 @@ public class MusicPlayer {
     }
 
     public void playMusic(){
-        System.out.println("Playing "+ music.getSong());
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
+
+//        System.out.println("Playing "+ music.getSong());
     }
 
     public void setMusic(Music music) {
