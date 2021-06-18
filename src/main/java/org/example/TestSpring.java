dd1d2f239b6b31af791156398fdf68a44d0e36b1
CopperAnt750 <opmailt@gmail.com>
2021-06-18 16:27:44 +0300

    Bean lifecycle

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -6,10 +6,12 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
+        ClassicalMusic classicalMusic = context.getBean("musicBean2", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
 //        Music music = context.getBean("musicBean",Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
-        musicPlayer.playMusic();
+/*        MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+        musicPlayer.playMusic();*/
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
         context.close();
